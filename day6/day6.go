d78d4b03f3b6381ac25d19f9e55def50d48df270
Eric Jean <eric.jean@monsanto.com>
2019-12-05 23:33:49 -0700

    Day 6 Part 2

--- day6/day6.go
+++ day6/day6.go
@@ -6,22 +6,9 @@ import (
 	"strings"
 )
 
-//type spaceObject struct {
-//	label string
-//	orbitedBy *spaceObject
-//}
-
-// B -> COM
-// C -> B
 func CountOrbits(orbitStrings []string) int {
+	orbitsMap := toOrbitsMap(orbitStrings)
 	orbits := 0
-	orbitsMap := make(map[string]string)
-	for _, orbitString := range orbitStrings {
-		parts := strings.Split(orbitString,")")
-		target := parts[0]
-		orbitedBy := parts[1]
-		orbitsMap[orbitedBy] = target
-	}
 	for _, target := range orbitsMap {
 		for {
 			orbits++
@@ -34,9 +21,73 @@ func CountOrbits(orbitStrings []string) int {
 	return orbits
 }
 
+func OrbitalTransfersNeeded(orbitStrings []string, from, to string) int {
+	orbitsMap := toOrbitsMap(orbitStrings)
+	lca := lowestCommonAncestor(orbitsMap, from, to)
+	fmt.Println("LCA is " + lca)
+
+	distance1 := distance(orbitsMap, orbitsMap[from], lca)
+	fmt.Printf("Orbital transfers from %s to %s = %d\n", from, lca, distance1)
+	distance2 := distance(orbitsMap, orbitsMap[to], lca)
+	fmt.Printf("Orbital transfers from %s to %s = %d\n", to, lca, distance2)
+
+	return distance1 + distance2
+}
+
+func toOrbitsMap(orbitStrings []string) map[string]string {
+	orbitsMap := make(map[string]string)
+	for _, orbitString := range orbitStrings {
+		parts := strings.Split(orbitString, ")")
+		target := parts[0]
+		orbitedBy := parts[1]
+		orbitsMap[orbitedBy] = target
+	}
+	return orbitsMap
+}
+
+func lowestCommonAncestor(orbitsMap map[string]string, from, to string) string {
+	pathToRootForFrom := make(map[string]bool)
+	objectName := from
+	for {
+		pathToRootForFrom[objectName] = true
+		if objectName == "COM" {
+			break
+		}
+		objectName = orbitsMap[objectName]
+	}
+
+	objectName = to
+	for {
+		if pathToRootForFrom[objectName] {
+			break
+		}
+		objectName = orbitsMap[objectName]
+	}
+
+	return objectName
+}
+
+func distance(orbitsMap map[string]string, from, to string) int {
+	result := 0
+
+	node := from
+	for {
+		if node == to {
+			break
+		}
+		node = orbitsMap[node]
+		result++
+	}
+
+	return result
+}
+
 func main() {
 	orbitStrings := adventutil.Parse(6)
 
 	numOrbits := CountOrbits(orbitStrings)
 	fmt.Printf("Part 1: %d\n", numOrbits)
+
+	orbitalTransfers := OrbitalTransfersNeeded(orbitStrings, "YOU", "SAN")
+	fmt.Printf("Part 12 %d\n", orbitalTransfers)
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -23,3 +23,26 @@ func TestCountOrbits(t *testing.T) {
 		t.Errorf("Expected 42, got %d", result)
 	}
 }
+
+func TestOrbitalTransfersNeeded(t *testing.T) {
+	input := []string {
+		"COM)B",
+		"B)C",
+		"C)D",
+		"D)E",
+		"E)F",
+		"B)G",
+		"G)H",
+		"D)I",
+		"E)J",
+		"J)K",
+		"K)L",
+		"K)YOU",
+		"I)SAN",
+	}
+	result := OrbitalTransfersNeeded(input, "YOU", "SAN")
+
+	if result != 4 {
+		t.Errorf("Expected 4, got %d", result)
+	}
+}
