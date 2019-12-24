b0c55f046733c63af36234d42d895cab35b839b6
Eric Jean <eric.jean@monsanto.com>
2019-12-23 21:49:50 -0700

    Day 16 Part 2

--- cmd/day16/day16.go
+++ cmd/day16/day16.go
@@ -6,22 +6,60 @@ import (
 	"strconv"
 )
 
-var pattern = [4]int{0, 1, 0, -1}
-
-func ApplyPhase(input []int) []int {
+func ApplyPhase(input []int, offset int) []int {
+	//fmt.Println("Applying phase")
+	totalLen := len(input)
 	var output []int
-	for outputDigit := 0; outputDigit < len(input); outputDigit++ {
+	for x :=0; x < offset; x++ {
+		output = append(output, 0)
+	}
+	for outputDigit := offset; outputDigit < totalLen; outputDigit++ {
+		//fmt.Printf("Digit %d = \n", outputDigit)
 		outputValue := 0
-		for i, val := range input {
-			patternIndex := ((i + 1) / (outputDigit + 1)) % 4
-			//fmt.Printf("%d*%d + ", val, pattern[patternIndex])
-			outputValue += val * pattern[patternIndex]
+		for i := outputDigit; i <= (outputDigit*2) && i < totalLen; i++ {
+			for j := i; j < totalLen; j += 4 * (outputDigit + 1) {
+				//fmt.Printf("+%d (input[%d])\n", input[j], j)
+				outputValue += input[j]
+			}
 		}
+		subtractStart := 2 + 3*outputDigit
+		for i := subtractStart; i <= subtractStart+outputDigit && i < totalLen; i++ {
+			for j := i; j < totalLen; j += 4 * (outputDigit + 1) {
+				//fmt.Printf("-%d (input[%d])\n", input[j], j)
+				outputValue -= input[j]
+			}
+		}
+		//fmt.Printf("total = %d\n", outputValue)
 		outputValue = adventutil.Abs(outputValue) % 10
 		//fmt.Printf(" = %d\n", outputValue)
 		output = append(output, outputValue)
 	}
 	return output
+}
+
+func ApplyPhase2(input []int, offset int) []int {
+	if offset < len(input) / 2 {
+		panic("Assumption violated")
+	}
+
+	result := make([]int, len(input))
+
+	sum := 0
+	for outputDigit := offset; outputDigit < len(input); outputDigit++ {
+		sum += input[outputDigit]
+	}
+	for outputDigit := offset; outputDigit < len(input); outputDigit++ {
+		result[outputDigit] = adventutil.Abs(sum) % 10
+		sum -= input[outputDigit]
+	}
+	return result
+}
+
+func Apply100(val []int, offset int, phaseFunc func([]int, int) []int) []int {
+	for i := 0; i < 100; i++ {
+		val = phaseFunc(val, offset)
+	}
+	return val
 }
 
 func parseSignal(input string) []int {
@@ -33,16 +71,31 @@ func parseSignal(input string) []int {
 	return output
 }
 
-func main() {
-	val := parseSignal(adventutil.Parse(16)[0])
-	for i := 0; i < 100; i++ {
-		val = ApplyPhase(val)
+func FullFFT(input string) string {
+	baseInputArray := parseSignal(input)
+	var inputArray []int
+	for i := 0; i < 10000; i++ {
+		inputArray = append(inputArray, baseInputArray...)
 	}
-
-	fmt.Printf("Part 1: ")
-	for i := 0; i < 8; i++ {
-		fmt.Printf("%d", val[i])
-	}
-	fmt.Println()
+	offset, _ := strconv.Atoi(input[:7])
+	result := Apply100(inputArray, offset, ApplyPhase2)
+	return DigitsAtOffset(result, offset)
 }
 
+func DigitsAtOffset(values []int, offset int) string {
+	var result string
+	for i := 0; i < 8; i++ {
+		result += fmt.Sprintf("%d", values[i+offset])
+	}
+	return result
+}
+
+func main() {
+	inputStr := adventutil.Parse(16)[0]
+	val := parseSignal(inputStr)
+	result := Apply100(val, 0, ApplyPhase)
+	fmt.Printf("Part 1: %s\n", DigitsAtOffset(result, 0))
+
+	part2 := FullFFT(inputStr)
+	fmt.Printf("Part 2: %s\n", part2)
+}
--- cmd/day16/day16_test.go
+++ cmd/day16/day16_test.go
@@ -8,14 +8,14 @@ import (
 func TestApplyPhase(t *testing.T) {
 	t.Run("Simple", func(t *testing.T) {
 		val := parseSignal("12345678")
-		expectedOutputs := []string {
+		expectedOutputs := []string{
 			"48226158",
 			"34040438",
 			"03415518",
 			"01029498",
 		}
 		for n, expected := range expectedOutputs {
-			val = ApplyPhase(val)
+			val = ApplyPhase(val, 0)
 			if asString(val) != expected {
 				t.Errorf("After %d phase expected %s got %s", n, expected, asString(val))
 			}
@@ -23,24 +23,39 @@ func TestApplyPhase(t *testing.T) {
 	})
 	t.Run("100 Run Tests", func(t *testing.T) {
 		tests := []struct {
-			input string
+			input    string
 			expected string
 		}{
-			{"80871224585914546619083218645595","24176176"},
+			{"80871224585914546619083218645595", "24176176"},
 			{"19617804207202209144916044189917", "73745418"},
 			{"69317163492948606335995924319873", "52432133"},
 		}
 		for _, test := range tests {
 			val := parseSignal(test.input)
-			for i := 0; i < 100; i++ {
-				val = ApplyPhase(val)
-			}
-			first8 := asString(val)[:8]
+			result := Apply100(val, 0, ApplyPhase)
+			first8 := asString(result)[:8]
 			if first8 != test.expected {
 				t.Errorf("Expected %s got %s", test.expected, first8)
 			}
 		}
 	})
+}
+
+func TestFullFFT(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"03036732577212944063491565474664", "84462026"},
+		{"02935109699940807407585447034323", "78725270"},
+		{"03081770884921959731165446850517", "53553731"},
+	}
+	for _, test := range tests {
+		result := FullFFT(test.input)
+		if result != test.expected {
+			t.Errorf("Expected %s got %s", test.expected, result)
+		}
+	}
 }
 
 func asString(intArray []int) string {
